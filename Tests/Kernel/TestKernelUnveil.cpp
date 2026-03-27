3ad6a686b030ec1f38f28a90b355fb58d4b23a02
Lucas Chollet <lucas.chollet@free.fr>
2026-03-27 08:47:26 +0100

    Tests/Kernel: Run unveil test in a separate process
    This will allow us to independently test multiple cases in the same
    file.

--- Tests/Kernel/TestKernelUnveil.cpp
+++ Tests/Kernel/TestKernelUnveil.cpp
@@ -39,53 +39,65 @@ TEST_CASE(test_argument_validation)
     EXPECT_EQ(errno, EINVAL);
 }
 
+static void run_in_other_process(auto&& test_case)
+{
+    EXPECT_NO_CRASH("unveil", [test_case = move(test_case)] {
+        test_case();
+        return Test::Crash::Failure::DidNotCrash;
+    });
+}
+
 TEST_CASE(test_failures)
 {
-    auto res = unveil("/etc", "r");
-    if (res < 0)
-        FAIL("unveil read only failed");
+    auto test = []() {
+        auto res = unveil("/etc", "r");
+        if (res < 0)
+            FAIL("unveil read only failed");
 
-    res = unveil("/etc", "w");
-    if (res >= 0)
-        FAIL("unveil write permitted after unveil read only");
+        res = unveil("/etc", "w");
+        if (res >= 0)
+            FAIL("unveil write permitted after unveil read only");
 
-    res = unveil("/etc", "x");
-    if (res >= 0)
-        FAIL("unveil execute permitted after unveil read only");
+        res = unveil("/etc", "x");
+        if (res >= 0)
+            FAIL("unveil execute permitted after unveil read only");
 
-    res = unveil("/etc", "c");
-    if (res >= 0)
-        FAIL("unveil create permitted after unveil read only");
+        res = unveil("/etc", "c");
+        if (res >= 0)
+            FAIL("unveil create permitted after unveil read only");
 
-    res = unveil("/tmp/doesnotexist", "c");
-    if (res < 0)
-        FAIL("unveil create on non-existent path failed");
+        res = unveil("/tmp/doesnotexist", "c");
+        if (res < 0)
+            FAIL("unveil create on non-existent path failed");
 
-    res = unveil("/home", "b");
-    if (res < 0)
-        FAIL("unveil browse failed");
+        res = unveil("/home", "b");
+        if (res < 0)
+            FAIL("unveil browse failed");
 
-    res = unveil("/home", "w");
-    if (res >= 0)
-        FAIL("unveil write permitted after unveil browse only");
+        res = unveil("/home", "w");
+        if (res >= 0)
+            FAIL("unveil write permitted after unveil browse only");
 
-    res = unveil("/home", "x");
-    if (res >= 0)
-        FAIL("unveil execute permitted after unveil browse only");
+        res = unveil("/home", "x");
+        if (res >= 0)
+            FAIL("unveil execute permitted after unveil browse only");
 
-    res = unveil("/home", "c");
-    if (res >= 0)
-        FAIL("unveil create permitted after unveil browse only");
+        res = unveil("/home", "c");
+        if (res >= 0)
+            FAIL("unveil create permitted after unveil browse only");
 
-    res = unveil(nullptr, nullptr);
-    if (res < 0)
-        FAIL("unveil state lock failed");
+        res = unveil(nullptr, nullptr);
+        if (res < 0)
+            FAIL("unveil state lock failed");
 
-    res = unveil("/bin", "w");
-    if (res >= 0)
-        FAIL("unveil permitted after unveil state locked");
+        res = unveil("/bin", "w");
+        if (res >= 0)
+            FAIL("unveil permitted after unveil state locked");
 
-    res = access("/bin/id", F_OK);
-    if (res == 0)
-        FAIL("access(..., F_OK) permitted after locked veil without relevant unveil");
+        res = access("/bin/id", F_OK);
+        if (res == 0)
+            FAIL("access(..., F_OK) permitted after locked veil without relevant unveil");
+    };
+
+    run_in_other_process(move(test));
 }
